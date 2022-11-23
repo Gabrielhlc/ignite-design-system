08e5900b02a3ab821a45178ab96930caa8ea2c94
Gabriel Carvalho <gabrielhenlc@gmail.com>
2022-11-23 16:25:00 -0300

    storybook controls and a11y

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
         src: 'https://github.com/Gabrielhlc.png',
         alt: 'Gabriel Carvalho',
     },
+    argTypes: {
+        src: {
+            control: {
+                type: 'text',
+            },
+        },
+    },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
